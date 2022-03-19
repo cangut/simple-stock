38c89a94065a0f8057af5f1fddd9760b0824f5d2
cangut <iamaswengineer@gmail.com>
2022-03-19 22:45:13 +0300

    STOCK010: query objects added

---
+++ cqrs-es/cqrs-es-core/src/main/java/com/can/simplestock/cqrsescore/query/BaseQuery.java
@@ -0,0 +1,4 @@
+package com.can.simplestock.cqrsescore.query;
+
+public abstract class BaseQuery {
+}
---
+++ query/src/main/java/com/can/simplestock/query/api/queries/FindAllStock.java
@@ -0,0 +1,7 @@
+package com.can.simplestock.query.api.queries;
+
+import com.can.simplestock.cqrsescore.query.BaseQuery;
+
+public class FindAllStock extends BaseQuery {
+
+}
