32192c81d5dc75db92f74653b9cfe4b8d77deffa
cangut <iamaswengineer@gmail.com>
2022-05-11 04:36:21 +0300

    STOCK015: package structure refactored.

--- cqrs-es-core/src/main/java/com/can/simplestock/cqrsescore/event/EventModel.java
+++ cqrs-es-core/src/main/java/com/can/simplestock/cqrsescore/event/EventModel.java
@@ -3,7 +3,6 @@ package com.can.simplestock.cqrsescore.event;
 import com.can.simplestock.cqrsescore.messages.BaseEvent;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
